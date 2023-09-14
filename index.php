12c25c02338121b74fdc460f300e5574b2bca08e
Carin Pretorius <carin@ceit.cc>
2023-09-14 05:31:13 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="generator" content="Mobirise v5.6.11, mobirise.com">
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:image:src" content="">
+        <meta property="og:image" content="">
+        <meta name="twitter:title" content="Home">
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+        <link rel="shortcut icon" href="assets/images/favicon.ico" type="image/x-icon">
+        <meta name="description" content="Die nuutste publikasies van Boekdiva Uitgewers, 'n uitgewery vir kontemporêre Afrikaanse skryfwerk, is op hierdie blad te sien.">
+
+        <title>Boekdiva Uitgewers</title>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+        <link rel="stylesheet" href="assets/animatecss/animate.css">
+        <link rel="stylesheet" href="assets/dropdown/css/style.css">
+        <link rel="stylesheet" href="assets/socicon/css/styles.css">
+        <link rel="stylesheet" href="assets/theme/css/style.css">
+        <link rel="preload"
+              href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,500,600,700,800,900,200i,300i,400i,500i,600i,700i,800i,900i&display=swap"
+              as="style" onload="this.onload=null;this.rel='stylesheet'">
+        <noscript>
+            <link rel="stylesheet"
+                  href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,500,600,700,800,900,200i,300i,400i,500i,600i,700i,800i,900i&display=swap">
+        </noscript>
+        <link rel="preload"
+              href="https://fonts.googleapis.com/css?family=Lora:400,500,600,700,400i,500i,600i,700i&display=swap"
+              as="style" onload="this.onload=null;this.rel='stylesheet'">
+        <noscript>
+            <link rel="stylesheet"
+                  href="https://fonts.googleapis.com/css?family=Lora:400,500,600,700,400i,500i,600i,700i&display=swap">
+        </noscript>
+        <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+        <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+        <meta property="og:title" content="Boekdiva Uitgewers" />
+        <meta property="og:description" content="Die nuutste publikasies van Boekdiva Uitgewers, 'n uitgewery vir kontemporêre Afrikaanse skryfwerk, is op hierdie blad te sien." />
+        <meta property='fb:app_id' content='701264160060226'/>
+		<meta property='og:url' content='https://boekdiva.co.za'/>
+		<meta property='og:type' content='book' />
+        <meta property="og:image" content="https://boekdiva.co.za/assets/images/logo_icon_w.jpg" />
+    </head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-N30QYTBGEQ"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-N30QYTBGEQ');
+        </script>
+    <body>
+
+    <section data-bs-version="5.1" class="menu menu1 top-menu-class" once="menu" id="menu1-1">
+        <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+            <div class="container">
+                <div class="navbar-brand">
+                    <span class="navbar-logo">
+                        <a href="boekdiva.co.za">
+                            <img src="assets/images/logo_b.png" alt="Boekdiva" style="height: 3.7rem;">
+                        </a>
+                    </span>
+                </div>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse"
+                        data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <div class="hamburger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
+                        <li class="nav-item">
+                            <a class="nav-link link text-warning display-4 obfuscate-it" href="#" data-name="info" data-domain="boekdiva" data-tld="co.za" data-mailto="true" data-content="true">KONTAK ONS</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </section>
+
+    <section class="header-class" id="header1-0">
+        <img src="assets/images/Heimwee.jpg" alt="Heinwee">
+    </section>
+
+    <section data-bs-version="5.1" class="features6 feature-book-class" id="features7-7">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg">
+                    <h1 class="d-none">Boekdiva Uitgewers</h1>
+                    <h2 class="d-none">Die nuutste publikasies van Boekdiva Uitgewers, 'n uitgewery vir kontemporêre Afrikaanse skryfwerk, is op hierdie blad te sien.</h2>
+                    <figure class="figure float-start m-4">
+                        <img src="assets/images/Heimwee_omslag.jpg" class="shadow" alt="Heimwee">
+                    </figure>
+                    <h5 class="mbr-title mbr-fonts-style display-2 mt-3">Heimwee</h5>
+                    <p class="mbr-text mbr-fonts-style display-7 text-start">
+                    <span class="fw-bold fs-4"><i>Heimwee</i></span> is die storie van Mart-Mari wat moet terugkeer na die familieplaas sodat sy haar ma kan
+                    begrawe en finaal kan afskeid neem van die familie by wie sy nooit tuis gevoel het nie. Maar in ŉ ondeurdagte oomblik betaal sy die
+                    knutselaar-nutsman-van-langsaan, Anton Nieuwoudt, om saam te gaan omdat sy vir haar susters gelieg het oor ŉ man in haar lewe.
+                    Gou besef sy dat hy hom nie laat gebruik nie. Hy is die alles-of-niks-tipe, wat haar dwing tot emosionele eerlikheid en die spieël
+                    voor haar ophou sodat sy bewus kan word van haar aandeel in haar gekompliseerde familieverhoudinge.<br><br>Dit is ook die storie van
+                    Liesbet Vermaak wat in die nadraai van die allergrootste vernedering alleen moet omsien na haar broers: Andries, die dromer, en
+                    Benna, die kind wat alles verstaan, maar nie met die wêreld kan kontak maak nie. Wat die foto van die soldaat steel sodat sy kan
+                    voorgee sy is getroud, maar dan op ŉ dag in Humansburg se hoofstraat opkyk in die helder heuning van Gert Niemand se oë en besef
+                    haar leuen het haar ingehaal.<br><br>Dis ŉ storie wat strek oor geslagte, van mense wat so seer gekry het dat hulle swerf, maar
+                    dis ook ŉ storie van heling. Boweal is dit die storie van moederskap, van die verskriklike prys wat dit soms vra, en van ma's wat
+                    bo-oor die ewigheid sal reik om seker te maak hulle kinders is geborge.
+                    </p>
+                    <div class="mbr-section-btn pt-3 float-end pe-5"><a href="read.php" class="btn btn-warning btn-sm display-4">Lees uittreksel</a></div>
+<!--                    <div class="mbr-section-btn pt-3"><a href="https://www.graffitibooks.co.za/af/386821/Boeke" target="_blank" class="btn btn-primary display-4">Koop nou</a></div>-->
+                </div>
+            </div>
+            <div class="row border-top mt-2 pb-2 pt-2 ps-5">
+                <div class="col-12 col-md-4 mbr-text mbr-fonts-style display-7 text-left">
+                    <b>ISBN</b>: 9780639704494
+                </div>
+                <div class="col-12 col-md-4 mbr-text mbr-fonts-style display-7 text-left">
+                    <b>Prys</b>: R350
+                </div>
+                <div class="col-12 col-md-4 mbr-text mbr-fonts-style display-7 text-left">
+                    <b>Publikasie datum</b>: Oktober 2022
+                </div>
+<!--            </div>-->
+<!--            <div class="row border-top border-bottom border-top mt-2 pb-2 pt-2 ps-5">-->
+                <div class="col-12 col-md-4 mbr-text mbr-fonts-style display-7 text-left">
+                    <b>Uitgewer</b>: Boekdiva Uitgewers
+                </div>
+                <div class="col-12 col-md-4 mbr-text mbr-fonts-style display-7 text-left">
+                    <b>Formaat</b>: Sagteband 198 x 129mm
+                </div>
+                <div class="col-12 col-md-4 mbr-text mbr-fonts-style display-7 text-left">
+                    <b>Bladsye</b>: 520
+                </div>
+            </div>
+            <div class="row ps-5 border-top">
+                <div class="col-12">
+                    <div class="mbr-section-btn pt-3">
+                        <a href="https://www.graffitibooks.co.za/af/386821/Boeke" target="_blank" class="btn btn-primary display-4">Koop nou - Graffiti Boeke</a>
+                        <a href="https://www.wordsworth.co.za/products/heimwee-paperback" target="_blank" class="btn btn-primary display-4">Koop nou - Wordsworth Books</a>
+                        <a href="https://www.exclusivebooks.co.za/product/9780639704494" target="_blank" class="btn btn-primary display-4">Koop nou - Exclusive Books</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section data-bs-version="5.1" class="footer4 footer-class" once="footers" id="footer4-3">
+        <div class="container">
+
+            <div class="row mbr-white">
+                <div class="col-10 col-md-6 col-lg-4">
+                    <div class="media-wrap col-4">
+                        <a href="index.php">
+                            <img src="assets/images/logo_w.png" alt="Boekdiva">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-10 col-md-6 col-lg-4">
+                    <h5 class="mbr-section-subtitle mbr-fonts-style mb-3 display-4 text-primary">
+                        <strong>Volg Ons</strong>
+                    </h5>
+                    <div class="social-row display-7 ps-2">
+                        <div class="soc-item">
+                            <a href="https://www.facebook.com/Boekdiva" target="_blank">
+                                <span class="mbr-iconfont socicon socicon-facebook"></span>
+                            </a>
+                        </div>
+<!--                        <div class="soc-item">-->
+<!--                            <a href="https://twitter.com/boekdiva" target="_blank">-->
+<!--                                <span class="mbr-iconfont socicon socicon-twitter"></span>-->
+<!--                            </a>-->
+<!--                        </div>-->
+                        <div class="soc-item">
+                            <a href="https://www.instagram.com/boekdivauitgewers/" target="_blank">
+                                <span class="mbr-iconfont socicon socicon-instagram"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-10 col-md-6 col-lg-4 pb-4 mb-4">
+                    <h5 class="mbr-section-subtitle mbr-fonts-style mb-2 display-4 text-primary">
+                        <strong>Kontak Ons</strong>
+                    </h5>
+                    <p class="mbr-text mbr-fonts-style mb-4 display-7 text-white">
+                        <a href="#" class="obfuscate-it text-white" data-name="info" data-domain="boekdiva" data-tld="co.za" data-mailto="true"></a>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+    </section>
+<?php
+include_once "parts/htmlEnd.php";
---
+++ parts/htmlEnd.php
@@ -0,0 +1,20 @@
+    <section><a href="https://mobiri/" style="display: none;"><a href="https://mobirise" style="display: none;">&nbsp;</a></section>
+    <script src="assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="assets/smoothscroll/smooth-scroll.js"></script>
+    <script src="assets/ytplayer/index.js"></script>
+    <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+    <script src="assets/theme/js/spamguard.js"></script>
+    <script src="assets/theme/js/script.js"></script>
+
+    <script>
+	spamguard(".obfuscate-it");
+    </script>
+
+    <div id="scrollToTop" class="scrollToTop mbr-arrow-up">
+        <a style="text-align: center;">
+            <i class="mbr-arrow-up-icon mbr-arrow-up-icon-cm cm-icon cm-icon-smallarrow-up"></i>
+        </a>
+    </div>
+    <input name="animation" type="hidden">
+    </body>
+</html>
